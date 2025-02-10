917b3ac2e1b6fe65373dd22be5e292093c2e1600
Fernando Carrion <32936851+pfcarrion@users.noreply.github.com>
2025-02-10 15:57:45 -0500

    Delete senales/core directory

--- senales/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Subscription
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'estado', 'fecha_inicio', 'fecha_fin', 'monto', 'es_recursiva', 'nombre', 'apellido')
-    list_filter = ('estado', 'es_recursiva')
-    search_fields = ('usuario__username','estado')
--- senales/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import secure_data_view
-from . import views
-
-
-urlpatterns = [
-    path('secure-data/', secure_data_view, name='secure_data'),
-    path('senales/', views.senales, name='senales'),
-    ]
